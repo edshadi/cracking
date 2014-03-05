9ba1a3a132002df06295dbe1a1ab8ab812bab704
Ed Shadi <edshadi@gmail.com>
2014-03-04 16:53:19 -0800

    hash table

---
+++ data-structures/hash_table.rb
@@ -0,0 +1,29 @@
+require_relative 'singly_linked_list'
+class HashTable
+  attr_reader :length, :map
+  def initialize
+    @map = []
+    @length = @map.length
+  end
+
+  def []= key, value
+    hashed_key = self.hash_func(key)
+    @length += 1
+    @map[hashed_key] = SinglyLinkedList.new([key, value]) and return unless @map[hashed_key]
+    @map[hashed_key].add([key, value])
+  end
+
+  def [] key
+    if list = @map[self.hash_func(key)]
+      current_node = list.current_node
+      until list.current_node.nil?
+        return list.current_node.value.last if list.current_node.value.first == key
+        list.next_node
+      end
+    end
+  end
+
+  def hash_func key
+    key.length % 10
+  end
+end
---
+++ data-structures/hash_table_spec.rb
@@ -0,0 +1,40 @@
+require 'rspec'
+require_relative 'hash_table'
+describe HashTable do
+  let(:key) { :a }
+  let(:value) { ":a value" }
+  let(:table) { HashTable.new }
+  context "#[]=" do
+    before(:each) do
+      table[key] = value
+    end
+    it "adds the value under the key" do
+      expect(table[key]).to eq value
+    end
+    it "values don't override each other override" do
+      table["a"] = "a value"
+      table["A"] = "A value"
+      expect(table[key]).to eq value
+    end
+  end
+  context "#[]" do
+    before(:each) do
+      table[key] = value
+    end
+    it "gets the value at the given key" do
+      expect(table[key]).to eq value
+    end
+  end
+
+  context "#hash" do
+    it "returns 0 if the array is empty" do
+      expect(table.hash_func(key)).to eq key.length % 10
+    end
+
+    it "mods the length of the array" do
+      table[key] = value
+      table["another_key"] = value
+      expect(table.hash_func(key)).to eq key.length % 10
+    end
+  end
+end
